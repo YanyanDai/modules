c1e00f8e66f038aa9b6f594dedfdafa8d6cc2250
YanyanDai <47199662+YanyanDai@users.noreply.github.com>
2022-01-26 12:11:41 +0900

    Add files via upload

---
+++ Robot_Motor_Control/SerialModule.py
@@ -0,0 +1,28 @@
+import serial
+import time
+
+def initconnection(portNo, baudRate):
+    try:
+        ser = serial.Serial(portNo, baudRate)
+        print("Device Connected")
+        return ser
+    except:
+        print("Not connected")
+
+def sendData(se,data,digits):
+    myString= "$"
+    for d in data:
+        myString += str(d).zfill(digits)
+    try:
+        se.write(myString.encode())
+        print(myString)
+    except:
+        print("Data transmission failed")
+
+if __name__ == "__main__":
+    ser = initconnection("/dev/ttyACM1", 115200)
+    while True:
+        sendData(ser,[0,255],3)
+        time.sleep(1)
+        sendData(ser,[0,0],3)
+        time.sleep(1)
